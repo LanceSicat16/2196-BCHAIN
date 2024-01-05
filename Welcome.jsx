8ad7a23086ed9424925315c092d4fa381c4cf4bf
LanceSicat16 <155714718+LanceSicat16@users.noreply.github.com>
2024-01-05 15:13:32 +0800

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    let message = "I think i've done it";
+    return(
+        <div>
+            <p>Welcome I am Lance Sicat!</p>
+            { /*<h1>{message}</h1>*/ }
+        </div>
+    ); 
+}
